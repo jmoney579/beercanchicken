567cf681650286932f45b4c834bba420f42950d1
Jacques Walker <jawalker@vt.edu>
2016-10-20 12:34:24 -0400

    added years to countdown timer, removed seconds

--- knot/assets/js/countup.js
+++ knot/assets/js/countup.js
@@ -7,12 +7,12 @@ function upTime(countTo) {
   countTo = new Date(countTo);
   difference = (now-countTo);
   days=Math.floor(difference/(60*60*1000*24)*1);
-  //years = Math.floor(days / 365);
-  //if (years > 1){ days = days - (years * 365)}
+  years = Math.floor(days / 365);
+  days = days - (years * 365);
   hours=Math.floor((difference%(60*60*1000*24))/(60*60*1000)*1);
   mins=Math.floor(((difference%(60*60*1000*24))%(60*60*1000))/(60*1000)*1);
   secs=Math.floor((((difference%(60*60*1000*24))%(60*60*1000))%(60*1000))/1000*1);
-  //document.getElementById('years').firstChild.nodeValue = years;
+  document.getElementById('years').firstChild.nodeValue = years;
   document.getElementById('days').firstChild.nodeValue = days;
   document.getElementById('hours').firstChild.nodeValue = hours;
   document.getElementById('minutes').firstChild.nodeValue = mins;
