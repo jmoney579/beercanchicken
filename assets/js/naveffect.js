86ff8969f8adcea5ddbb11b31f7acae1efcdebf9
Jacques <jawalker@vt.edu>
2016-12-28 23:35:31 -0500

    added recent updates section with js, added icons into slim nav

--- assets/js/naveffect.js
+++ assets/js/naveffect.js
@@ -2,9 +2,11 @@
 			$(".site_title").click(function(){
 				if ($(".navbar").is(":hidden")){
 						$(".navbar").fadeIn();
+						$(".updates").fadeIn();
 						$(".site_title").addClass("slide");
 					}else{
 						$(".navbar").fadeOut();
+						$(".updates").fadeOut();
 						$(".site_title").removeClass("slide");
 							}
 				});
@@ -15,4 +17,5 @@
 						$(".navbar").fadeOut();
 							}
 				})
+			$(".updates").append('<h3>Recent Updates</h3><ul><li><a href="#">Multimedia</a></li><li><a href="#">National Adoption Month</a></li><li><a href="#">VDSS Annual Report</a></li><li><a href="#">VDSS Career Campaign</a></li></ul>');
 			});
