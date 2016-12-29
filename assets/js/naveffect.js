8607b5061aa3beac81dd8d99f9f00e20db00ba12
Jacques <jawalker@vt.edu>
2016-12-28 20:50:56 -0500

    added subtitle, adjusted text padding, added nav and scroll effects

--- assets/js/naveffect.js
+++ assets/js/naveffect.js
@@ -2,8 +2,10 @@
 			$(".site_title").click(function(){
 				if ($(".navbar").is(":hidden")){
 						$(".navbar").fadeIn();
+						$(".site_title").addClass("slide");
 					}else{
 						$(".navbar").fadeOut();
+						$(".site_title").removeClass("slide");
 							}
 				});
 			$(".super_title").click(function(){
--- assets/js/scroll_effect.js
+++ assets/js/scroll_effect.js
@@ -1,20 +1,16 @@
 $(document).ready(function(){
-		var i = 0;
+		/*var i = 0;
 		$(".super_title").each(function(){
 		i++;
 		$(this).addClass('div_'+i);
-		});
+		});*/
 	   $(window).bind('scroll', function() {
-	   var navHeight = $( window ).height() - 100;
+	   var navHeight = $( window ).height() - 600;
 			 if ($(window).scrollTop() > navHeight) {
 				 $('.sticky').addClass('snaptotop');
-				 $('.top_content').addClass('stomped');
-				 $('.super_title').addClass('stomped2');
 			 }
 			 else {
-				 $('.sticky').removeClass('snaptotop');
-				 $('.top_content').removeClass('stomped');
-				 $('.super_title').removeClass('stomped2');		 
+				 $('.sticky').removeClass('snaptotop');		 
 			 }
 				 
 		});
