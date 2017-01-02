8c7da8472181a9feb16839e54681a8feea8a174d
Jacques <jawalker@vt.edu>
2017-01-01 20:29:22 -0500

    updated recent updates, reduced scrol needed for footer display, added text for portfolio 4.0

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -25,7 +25,7 @@
 		var imagePos = $(this).offset().top;
 
 		var topOfWindow = $(window).scrollTop();
-			if (imagePos < topOfWindow+600) {
+			if (imagePos < topOfWindow+650) {
 				$(this).addClass("slideUp");
 			}
 		});
