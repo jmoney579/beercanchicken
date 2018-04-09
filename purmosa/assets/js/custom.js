d48fcfbf17f3cd9d322ef8f4173605d613b45b09
Jacques Walker <jawalker@vt.edu>
2018-04-09 15:29:02 -0400

    added styling fixes to footer

--- purmosa/assets/js/custom.js
+++ purmosa/assets/js/custom.js
@@ -4,3 +4,18 @@ $("#main_logo").delay(1000).fadeOut(1500,function(){
     });
     $("#main_logo").attr("src","assets/img/purmosa_logo_home.png");
 });
+
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
